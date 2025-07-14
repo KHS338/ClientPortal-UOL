4415d82ef550b39bd14df037a54bb55fbd007192
Usman Rasool <usmanrasool717@gmail.com>
2025-07-14 19:56:22 +0500

    Sidebar added

---
+++ src/app/dashboard/layout.js
@@ -0,0 +1,15 @@
+// app/dashboard/layout.js
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
+
+export default function DashboardLayout({ children }) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main>
+        <SidebarTrigger />
+        {children}
+      </main>
+    </SidebarProvider>
+  )
+}
