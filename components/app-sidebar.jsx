7b2ff1423af73dcaf636c02b8e00593053fee96a
Hamza <hamzasajjad338@gmail.com>
2025-07-16 11:54:50 +0500

    Revert "build three pages 1.cvSourcing 2.preQualification 3.360/direct"
    This reverts commit 8e2b62d75b46c3e23f38b0c0f41c2ec4f569baff.

--- components/app-sidebar.jsx
+++ components/app-sidebar.jsx
@@ -1,5 +1,5 @@
 import { UserCircleIcon,CircleDollarSign,LaptopMinimalCheck,Calendar, Home, Inbox, Search, Settings, DollarSignIcon, User } from "lucide-react"
- 
+
 import {
   Sidebar,
   SidebarContent,
@@ -10,37 +10,29 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar"
- 
+
 // Menu items.
 const items = [
   {
     title: "Dashboard",
-    url: "/dashboard",
+    url: "#",
     icon: Home,
   },
   {
     title: "Manage Roles",
+    url: "/roles",
     icon: LaptopMinimalCheck,
-    submenu: [
-      {
-        title: "CV Sourcing",
-        url: "/roles/cvSourcing",
-      },
-      {
-        title: "PreQualification",
-        url: "/roles/PreQualification",
-      },
-      {
-        title: "360/Direct",
-        url: "/roles/360Direct",
-      },
-    ],
   },
   {
     title: "Invoices",
     url: "/invoice",
     icon: CircleDollarSign,
   },
+//   {
+//     title: "Search",
+//     url: "#",
+//     icon: Search,
+//   },
   {
     title: "Profile",
     url: "/profile",
@@ -49,8 +41,6 @@ const items = [
 ]
 
 export function AppSidebar() {
-  const [openMenu, setOpenMenu] = useState(null)
-
   return (
     <Sidebar>
       <SidebarContent>
@@ -61,9 +51,9 @@ export function AppSidebar() {
               {items.map((item) => (
                 <SidebarMenuItem key={item.title}>
                   <SidebarMenuButton asChild>
-                    <a
+                    <a 
                       href={item.url}
-                      className="group flex items-center gap-3 rounded-lg px-3 py-2.5 text-sm font-medium transition-all duration-300 ease-in-out hover:bg-gradient-to-r hover:from-[#158A15] hover:to-[#0F6B0F] hover:text-white hover:shadow-xl hover:scale-[1.02] active:scale-[0.98] focus:outline-none focus:ring-2 focus:ring-[#19AF1A] focus:ring-offset-2"
+                      className="group flex items-center gap-3 rounded-lg px-3 py-2.5 text-sm font-medium transition-all duration-200 ease-in-out hover:bg-gradient-to-r hover:from-green-500 hover:to-green-600 hover:text-white hover:shadow-lg hover:scale-[1.02] active:scale-[0.98] focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 lg:text-2xl "
                     >
                       <item.icon className="h-5 w-5 transition-transform duration-200 group-hover:scale-110" />
                       <span className="transition-all duration-200">{item.title}</span>
